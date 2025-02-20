f86c3537bc3d75f5571efa0f354f6a6c1c933430
Joohyun Kim <148737398+kimbobby@users.noreply.github.com>
2025-02-20 20:04:55 +0900

    Feat: tailwind.config.ts 셋팅 및 버튼 컴포넌트 구현 (#2)
    * Chore: next.js 기본 파일 삭제
    
    * Chore: tailwind.config.ts 셋팅 및 pretendard 전역 폰트 적용
    
    * Feat: buttonBase 구현
    
    * Feat: buttonBase 및 tailwind.config.ts 수정
    
    * Feat: buttonFilled 컴포넌트 구현
    
    - buttonBase 컴포넌트 기반으로 구현
    - Primary 버튼: 전체 너비, 맞춤 너비, 작은 크기 버전 구현, colorNum prop을 통한 버튼 색상 음영 조절 기능 추가
    - Gray 버튼: 전체 너비, 맞춤 너비, 고정 너비 버전 추가
    
    * Feat: buttonBase 컴포넌트 icon gap 추가
    
    * Feat: buttonOutlined 컴포넌트 구현
    
    * Fix: tailwind.config.ts content 설정에 color.ts 경로 추가
    
    buttonBase에서 background 컬러를 정의한 BACKGROUND 객체를 lib/constants 폴더에 분리해 저장한 뒤에  커스텀한 컬러들이 렌더링 화면에 표시되지 않는 현상이 생김
    
    원인: tailwind css Content 설정에 해당 경로가 누락되어 유효파일 범위에 포함되지 않았고 빌드에서 제외됨
    white의 경우 tailwind 기본 색상으로 포함되어 표시됨
    
    해결: tailwind.config.ts content 설정에 color.ts 경로 추가

---
+++ src/app/(components)/commons/button/buttonOutlined.tsx
@@ -0,0 +1,46 @@
+import ButtonBase from "./buttonBase";
+import { IButtonFilledProps, buttonLargeHeight, buttonSmallHeight } from "./buttonFilled";
+
+interface IButtonOutlinedProps extends Omit<IButtonFilledProps, "colorNum"> {}
+
+const buttonOutlinedBase = `text-center rounded-full leading-none flex justify-center items-center border `;
+const outlinePrimary = "border-primary-100 text-primary-100";
+const outlineGray = "border-gray-100 text-black-100";
+
+export function ButtonOutlinedPrimaryLFull({ children, ...rest }: IButtonOutlinedProps) {
+  return (
+    <ButtonBase
+      bgColor="white"
+      className={`${buttonOutlinedBase} ${buttonLargeHeight} ${outlinePrimary} w-full py-3 font-semibold text-body-1`}
+      {...rest}
+    >
+      {children}
+    </ButtonBase>
+  );
+}
+
+export function ButtonOutlinedPrimaryLFit({ children, ...rest }: IButtonOutlinedProps) {
+  return (
+    <ButtonBase
+      bgColor="white"
+      className={`${buttonOutlinedBase} ${buttonLargeHeight} ${outlinePrimary} w-fit py-3 px-4 font-semibold text-body-1`}
+      {...rest}
+    >
+      {children}
+    </ButtonBase>
+  );
+}
+
+export function ButtonOutlinedGraySFit({ children, iconTrailing, ...rest }: IButtonOutlinedProps) {
+  return (
+    <ButtonBase
+      bgColor="white"
+      className={`${buttonOutlinedBase}  ${buttonSmallHeight} ${outlineGray} w-fit py-2 ${
+        iconTrailing ? "pl-3 pr-2" : "px-3"
+      } font-medium text-label-1`}
+      {...rest}
+    >
+      {children}
+    </ButtonBase>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,58 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/constants/colors.ts",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        gray: {
+          100: "#a8a8a8",
+          200: "#dbdbdb",
+          300: "#f6f6f6",
+        },
+        black: {
+          100: "#363636",
+          200: "#505050",
+          300: "#767676",
+        },
+        white: "#FFF",
+        primary: {
+          100: "#6F4D3F",
+          200: "#A47764",
+          300: "#CB9C88",
+          400: "#DBC0B0",
+          500: "#EAD8C4",
+          600: "#F3EDE7",
+        },
+        red: "#ed1c24",
+      },
+      fontSize: {
+        "headline-1": "36px",
+        "headline-2": "28px",
+        "headline-3": "24px",
+        "title-1": "20px",
+        "title-2": "18px",
+        "body-1": "16px",
+        "body-2": "15px",
+        "label-1": "14px",
+        "label-2": "13px",
+        "label-3": "12px",
+      },
+      fontFamily: {
+        pretendard: ["var(--font-pretendard)"],
+      },
+    },
+    screens: {
+      mobile: "320px",
+      tablet: "768px",
+      pc: "1200px",
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
