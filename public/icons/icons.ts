a7b455d7c5d0e7391eb8526dd6394d8ea40e0f0c
Joohyun Kim <trust987@naver.com>
2025-03-06 19:10:19 +0900

    Chore: 아이콘 svg 저장 및 ICONS 정리

---
+++ public/icons/icons.ts
@@ -0,0 +1,33 @@
+import ArrowDownGray from "./ArrowDownGray.svg";
+import ArrowDownPrimary from "./ArrowDownPrimary.svg";
+
+import BookmarkOutlined from "./BookmarkOutlined.svg";
+
+import Close from "./Close.svg";
+import CheckGray from "./CheckGray.svg";
+import Pen from "./Pen.svg";
+import Filter from "./Filter.svg";
+
+import Link from "./Link.svg";
+import Pin from "./Pin.svg";
+import Globe from "./Globe.svg";
+
+import Home from "./Home.svg";
+import Activity from "./Activity.svg";
+import Collection from "./Collection.svg";
+
+export {
+  ArrowDownGray,
+  ArrowDownPrimary,
+  Filter,
+  BookmarkOutlined,
+  Close,
+  CheckGray,
+  Pen,
+  Link,
+  Pin,
+  Globe,
+  Home,
+  Activity,
+  Collection,
+};
