9186169df6f9af661d9bab48c4c9355234779476
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-03-07 14:06:44 +0900

    Feat: ModalPortal 구축 및 ModalContainer 컴포넌트 추가

---
+++ src/components/modal/ModalContainer.tsx
@@ -0,0 +1,20 @@
+import ModalPortal from "@/lib/portal/ModalPortal";
+import { useRef } from "react";
+
+interface ModalProps {
+  children: React.ReactNode;
+}
+
+export const ModalContainer = ({ children }: ModalProps) => {
+  const ref = useRef(null);
+
+  return (
+    <ModalPortal>
+      <div className="flex justify-center items-center w-full h-full fixed top-0 left-0 z-50 bg-black-300 opacity-50">
+        <div ref={ref} className="flex flex-col justify-center items-center bg-white rounded-xl">
+          {children}
+        </div>
+      </div>
+    </ModalPortal>
+  );
+};
