0aa097981f3afa0af5c6c156ee44db77b8fbea99
김하은 <han.kim0904@gmail.com>
2025-03-25 19:07:02 +0900

    Fix: 빌드 에러 고침

--- src/app/perfume/[id]/page.tsx
+++ src/app/perfume/[id]/page.tsx
@@ -1,16 +1,16 @@
 import { SearchBar } from "@/components/commons/search/SearchBar";
 
-interface PerfumeDetailPageProps {
-  params: { id: string };
-}
-
-export default function PerfumeDetailPage({ params }: PerfumeDetailPageProps) {
-  const { id } = params;
+export default async function PerfumeDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const resolvedParams = await params;
 
   return (
     <main>
       <h1>향수 상세 페이지</h1>
-      <p>향수 ID: {id}</p>
+      <p>향수 ID: {resolvedParams.id}</p>
       <SearchBar />
     </main>
   );
