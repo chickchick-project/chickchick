0d655bba9f198800bfabd61232d47a6d74fc4292
Joohyun Kim <trust987@naver.com>
2025-05-01 01:52:53 +0900

    Feat: 커뮤니티 페이지 퍼블리싱

---
+++ src/app/community/post/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <>
+      <h1>커뮤니티 상세</h1>
+    </>
+  );
+}
---
+++ src/components/domains/community/PageClient.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useState } from "react";
+import { COMMUNITY_BOARDS } from "@/lib/constants/communityBoard";
+import { Header } from "./header";
+import CommunityCards from "./communityCards";
+import { communityPostData } from "./communityCards/dummyCommunityCards";
+
+export default function PageClient() {
+  const boards = Object.entries(COMMUNITY_BOARDS).map(([key, value]) => ({
+    key,
+    label: value,
+  }));
+  const [selectedTab, setSelectedTab] = useState(boards[0].key);
+
+  const handleTabClick = (key: string) => {
+    setSelectedTab(key);
+  };
+  //임시 목데이터
+  const postData =
+    selectedTab === "best" ? communityPostData : communityPostData.filter((post) => post.categoryType === selectedTab);
+
+  return (
+    <div className="w-full flex flex-col items-center gap-5">
+      <Header boards={boards} selectedTab={selectedTab} handleTabClick={handleTabClick} />
+      <main className="pb-[280px]">
+        <CommunityCards postData={postData} selectedTab={selectedTab} />
+      </main>
+    </div>
+  );
+}
