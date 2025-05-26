e6c55ad6263096fa325013d7ba03f2077f3b5ced
kuan <guen9310@gmail.com~>
2025-05-27 02:41:10 +0900

    Feat : 로그아웃 처리를 위해 form action -> onSubmit 으로 변경

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { useModalStore } from "@/lib/stores/useModalStore";
+import { MODAL_KEYS } from "@/lib/stores/useModalStore";
+import { LoginModal } from "@/components/modal/LoginModal";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  const { loginModal, closeModal } = useModalStore();
+
+  return (
+    <>
+      {children}
+      {loginModal && (
+        <LoginModal closeModal={() => closeModal(MODAL_KEYS.LOGIN)} />
+      )}
+    </>
+  );
 }
--- src/components/commons/navBar/UserProfileSection.tsx
+++ src/components/commons/navBar/UserProfileSection.tsx
@@ -5,49 +5,18 @@ import { useUserStore } from "@/lib/stores/useUserStore";
 import { NAV_LABELS } from "./navBar.constants";
 import ICONS from "@/lib/constants/icons";
 import { NavDropdown } from "./Dropdown";
-import { LoginModal } from "@/components/modal/LoginModal";
+import { useModalStore, MODAL_KEYS } from "@/lib/stores/useModalStore";
 
 const UserProfileSection = () => {
-  const [openModal, setOpenModal] = useState(false);
   const navBarRef = useRef<HTMLDivElement | null>(null);
-  const { user, isLoading: isZustandLoading } = useUserStore();
-
+  const { user, isLoading } = useUserStore();
+  const { openModal } = useModalStore();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  let displayContent = null;
-  console.log(user);
-  if (!user) {
-    displayContent = (
-      <div className="w-[120px]">
-        <button
-          className="text-body-2 font-medium text-black-300"
-          onClick={() => {
-            setOpenModal(true);
-          }}
-        >
-          {NAV_LABELS.LOGIN}
-        </button>
-        {openModal && <LoginModal closeModal={() => setOpenModal(false)} />}
-      </div>
-    );
-  } else {
-    displayContent = (
-      <div className="flex items-center gap-2">
-        <Image
-          src={user?.imageUrl ? user.imageUrl : "/images/profile.svg"}
-          className="rounded-full"
-          width={36}
-          height={36}
-          alt="User"
-        />
-        <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
-          <Image
-            src={ICONS.ArrowDownGray.src}
-            width={16}
-            height={16}
-            alt="Down"
-          />
-        </button>
+  if (isLoading) {
+    return (
+      <div className="w-[120px] pl-6 flex items-center justify-center h-[36px]">
+        <div className="animate-pulse h-8 w-20 bg-white rounded" />
       </div>
     );
   }
@@ -55,17 +24,42 @@ const UserProfileSection = () => {
   return (
     <div
       ref={navBarRef}
-      className={`w-[120px] pl-6 relative flex items-center gap-2 transition-opacity duration-300 ${
-        !isZustandLoading ? "opacity-100 visible" : "opacity-0 invisible"
-      }`}
+      className="w-[120px] pl-6 relative flex items-center gap-2"
     >
-      {displayContent}
-      {isDropdownOpen && navBarRef.current && (
-        <NavDropdown
-          onClose={() => setIsDropdownOpen(false)}
-          parentRef={navBarRef}
-          userId={user!.id}
-        />
+      {!user ? (
+        <div className="w-full">
+          <button
+            className="text-body-2 font-medium text-black-300"
+            onClick={() => openModal(MODAL_KEYS.LOGIN)}
+          >
+            {NAV_LABELS.LOGIN}
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center gap-2">
+          <Image
+            src={user.imageUrl || "/images/profile.svg"}
+            className="rounded-full"
+            width={36}
+            height={36}
+            alt="User"
+          />
+          <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            <Image
+              src={ICONS.ArrowDownGray.src}
+              width={16}
+              height={16}
+              alt="Down"
+            />
+          </button>
+          {isDropdownOpen && navBarRef.current && (
+            <NavDropdown
+              onClose={() => setIsDropdownOpen(false)}
+              parentRef={navBarRef}
+              userId={user.id}
+            />
+          )}
+        </div>
       )}
     </div>
   );
