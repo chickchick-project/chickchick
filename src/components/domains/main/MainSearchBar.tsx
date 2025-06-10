2d5519880f7c6d9805c5af94f9dec33ea3c05e36
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-10 09:39:35 +0900

    Chore: MainSearchBar에서 테스트를 위해 추가했던 리뷰 모달 관련 코드 제거

--- src/components/domains/main/MainSearchBar.tsx
+++ src/components/domains/main/MainSearchBar.tsx
@@ -1,28 +1,11 @@
 "use client";
 
 import { SearchBar } from "@/components/commons/search/SearchBar";
-import { ReviewModal } from "@/components/modal/ReviewModal";
-import { useState } from "react";
 
 export const MainSearchBar = () => {
-  const [open, setOpen] = useState(false);
   return (
     <div className="flex justify-center items-center py-10">
       <SearchBar />
-      <button
-        onClick={() => {
-          setOpen(true);
-        }}
-      >
-        리뷰 모달
-      </button>
-      {open && (
-        <ReviewModal
-          closeModal={() => {
-            setOpen(false);
-          }}
-        />
-      )}
     </div>
   );
 };
