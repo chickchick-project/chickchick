06d016c705648a6636c973ee25933a0423228d3b
Joohyun Kim <trust987@naver.com>
2025-06-12 12:59:14 +0900

    Feat: post editor ckeditor5로 변경

--- src/components/domains/post/form/PostEditor.tsx
+++ src/components/domains/post/form/PostEditor.tsx
@@ -1,11 +1,16 @@
+"use client";
+import dynamic from "next/dynamic";
 import SubTitleLabel from "./element/SubTitleLabel";
-
+import EditorLoading from "@/components/commons/ckeditor5/EditorLoading";
+const CkEditor5 = dynamic(
+  () => import("@/components/commons/ckeditor5/CkEditor5"),
+  { ssr: false, loading: () => <EditorLoading /> }
+);
 export default function PostEditor() {
   return (
     <>
       <SubTitleLabel label="내용" htmlFor="community-editor" isRequired />
-      {/* 텍스트 에디터로 변경예정 */}
-      <textarea id="community-editor" className="border border-gray-100" />
+      <CkEditor5 id="community-editor" onChange={() => {}} />
     </>
   );
 }
