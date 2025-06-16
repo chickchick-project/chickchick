dd01cd8f0155cffae4c504e542f619f3b167cf3c
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-16 14:33:10 +0900

    Design: MainContent 컴포넌트 구조 개선 및 반응형 스타일 적용

--- src/components/domains/main/mainContent/MainContent.tsx
+++ src/components/domains/main/mainContent/MainContent.tsx
@@ -1,15 +1,22 @@
-import { MainContentButtons } from "./MainContentButton";
+import { MainContentButtons } from "./MainContentButton/MainContentButtons";
+import { MainContentButtonsSwiper } from "./MainContentButton/MainContentButtonsSwiper";
 import { MainContentReview } from "./MainContentReview";
 import { MainContentCommunity } from "./mainContentCommunity/MainContentCommunity";
 
 export const MainContent = () => {
   return (
-    <section className="h-fit flex items-start justify-center gap-8 px-4 pb-10">
-      <div className="w-fit h-full flex flex-col gap-5">
-        <MainContentButtons />
-        <MainContentReview />
+    <section className="w-full flex items-center justify-center">
+      <div className="tablet:max-w-[1200px] h-fit flex tablet:flex-row flex-col items-start justify-center gap-8 px-4 pb-10">
+        <div className="tablet:flex hidden h-full flex-col gap-5">
+          <MainContentButtons />
+          <MainContentReview />
+        </div>
+        <div className="tablet:hidden flex flex-col gap-10">
+          <MainContentButtonsSwiper />
+          <MainContentReview />
+        </div>
+        <MainContentCommunity />
       </div>
-      <MainContentCommunity />
     </section>
   );
 };
