7ae0299202695692d8b711ace42dc0f00220ea23
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-16 14:32:03 +0900

    Design: MainContentReview 반응형 스타일 적용

--- src/components/commons/card/reviewCard/reviewCard.helpers.ts
+++ src/components/commons/card/reviewCard/reviewCard.helpers.ts
@@ -2,7 +2,8 @@ const getLayoutSize = (isMyPage: boolean) => {
   return {
     articleSize: isMyPage
       ? "w-[504px] h-[220px]"
-      : "w-[320px] h-[162px] tablet:w-[704px] tablet:h-[248px]",
+      : "w-full tablet:h-[220px] h-fit",
+    // "w-[320px] h-[162px] tablet:w-[704px] tablet:h-[248px]", // 모바일 환경의 리뷰 카드 사이즈 변경(임시)
     imageSize: isMyPage
       ? { width: 180, height: 180 }
       : { width: 200, height: 200 },
