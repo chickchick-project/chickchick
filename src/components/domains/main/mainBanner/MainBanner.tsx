0e6a093eeb2ec709a6a34c7841908fd5b6e2593c
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-16 14:30:24 +0900

    Design: MainBanner 반응형 스타일 적용

--- src/components/domains/main/mainBanner/MainBanner.tsx
+++ src/components/domains/main/mainBanner/MainBanner.tsx
@@ -1,4 +1,3 @@
-import { Headline2Semibold } from "@/components/commons/text/Headline2Semibold";
 import { Perfume } from "@/app/api/search/route";
 import { MainBannerPerfumeList } from "./MainBannerPerfumeList";
 
@@ -8,9 +7,11 @@ interface IBannerProps {
 
 export const MainBanner = ({ data }: IBannerProps) => {
   return (
-    <section className="flex flex-col items-center justify-center px-10 py-9 bg-gray-300 w-full">
-      <div className="flex flex-col justify-center items-start gap-5 w-full pc:w-[1200px]">
-        <Headline2Semibold>추운 겨울에 어울리는 향수 TOP 5</Headline2Semibold>
+    <section className="flex flex-col items-center justify-center tablet:px-10 px-5 tablet:py-9 py-5 bg-gray-300 w-full">
+      <div className="flex flex-col justify-center items-start tablet:gap-5 gap-4 w-full pc:w-[1200px]">
+        <div className="tablet:text-headline-2 text-title-2 font-semibold text-black-100">
+          추운 겨울에 어울리는 향수 TOP 5
+        </div>
         {data && <MainBannerPerfumeList data={data} />}
       </div>
     </section>
