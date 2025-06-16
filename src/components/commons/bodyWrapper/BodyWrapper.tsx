a8d714be1bcc94f0f543e2ae3d70dc7ca9f3cf9b
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-16 17:25:03 +0900

    Feat: BottomNavBar 추가 및 적용
    BottomNavBar가 mypage에서 제대로 적용되지 않는 문제 해결 필요.

--- src/components/commons/bodyWrapper/BodyWrapper.tsx
+++ src/components/commons/bodyWrapper/BodyWrapper.tsx
@@ -1,11 +1,17 @@
 "use client";
-import { usePathname } from "next/navigation";
 
+import { usePathname } from "next/navigation";
+import { BottomNavBar } from "../bottomNavBar";
+
+// TODO: BottomNavBar가 mypage에서 제대로 적용되지 않는 문제 해결 필요.
 export const BodyWrapper = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const isHome = pathname === "/";
 
   return (
-    <div className={isHome ? "" : "max-w-[1200px] mx-auto"}>{children}</div>
+    <div className={`${isHome ? "" : "max-w-[1200px] mx-auto"} relative`}>
+      {children}
+      <BottomNavBar />
+    </div>
   );
 };
