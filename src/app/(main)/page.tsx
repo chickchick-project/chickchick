48e11e655f2345988afb76f189e08c9d5b792f3a
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-17 16:40:21 +0900

    Refactor: Home 컴포넌트에서 fetchPerfumesWithBanner 함수 호출을 prisma.perfume.findMany로 변경

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,10 +2,30 @@ import { MainBanner } from "@/components/domains/main/mainBanner/MainBanner";
 import { MainContent } from "@/components/domains/main/mainContent/MainContent";
 import { MainLogo } from "@/components/domains/main/MainLogo";
 import { MainSearchBar } from "@/components/domains/main/MainSearchBar";
-import { fetchPerfumesWithBanner } from "@/lib/queries/perfumeQueries";
+import { prisma } from "@/lib/prisma";
 
+// TODO: 배너에 보여줄 테마에 맞는 필터 적용 필요
 export default async function Home() {
-  const data = await fetchPerfumesWithBanner(1, 5);
+  const data = await prisma.perfume.findMany({
+    select: {
+      id: true,
+      nameKo: true,
+      nameEn: true,
+      imageUrl: true,
+      brand: {
+        select: {
+          id: true,
+          nameKo: true,
+          nameEn: true,
+        },
+      },
+    },
+    take: 5,
+    skip: 1,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return (
     <>
