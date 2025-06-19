320e1fe2d837459580c10b2a675b989daf84af91
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-19 17:23:38 +0900

    Fix: ModalContainer 컴포넌트에 overlayClassName 프로퍼티 추가 및 구조 개선

--- src/components/modal/ModalContainer.tsx
+++ src/components/modal/ModalContainer.tsx
@@ -7,12 +7,14 @@ interface ModalProps {
   children: React.ReactNode;
   closeModal: () => void;
   className?: string;
+  overlayClassName?: string;
 }
 
 export const ModalContainer = ({
   children,
   closeModal,
   className,
+  overlayClassName,
 }: ModalProps) => {
   const ref = useRef(null);
 
@@ -26,21 +28,28 @@ export const ModalContainer = ({
 
   return (
     <ModalPortal>
-      <div className="flex justify-center items-center w-full h-full fixed top-0 left-0 z-50 bg-black-300 bg-opacity-90">
+      <div
+        className={`flex justify-center ${
+          overlayClassName || "items-center"
+        } w-full h-full fixed top-0 left-0 z-50 bg-black-300 bg-opacity-90`}
+      >
         <div
           ref={ref}
           className={`flex flex-col justify-center items-center relative bg-white rounded-xl ${className}`}
         >
-          <button onClick={closeModal}>
-            <Image
-              className="absolute top-5 right-5"
-              src={ICONS.Close.src}
-              alt={ICONS.Close.alt}
-              width={24}
-              height={24}
-            />
-          </button>
-          {children}
+          <div className="w-full h-full relative">
+            <button onClick={closeModal}>
+              <Image
+                className="absolute top-5 right-5"
+                src={ICONS.Close.src}
+                alt={ICONS.Close.alt}
+                width={24}
+                height={24}
+              />
+            </button>
+
+            {children}
+          </div>
         </div>
       </div>
     </ModalPortal>
