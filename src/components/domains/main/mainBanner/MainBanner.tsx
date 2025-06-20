f1e19f2966cd32c63cfbc6553b1850b97800a4a4
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-06-20 11:23:21 +0900

    Design: 메인 배너 및 향수 리스트 컴포넌트 스타일 수정

--- src/components/domains/main/mainBanner/MainBanner.tsx
+++ src/components/domains/main/mainBanner/MainBanner.tsx
@@ -8,7 +8,7 @@ interface IBannerProps {
 export const MainBanner = ({ data }: IBannerProps) => {
   return (
     <section className="flex flex-col items-center justify-center tablet:px-10 px-5 tablet:py-9 py-5 bg-gray-300 w-full">
-      <div className="flex flex-col justify-center items-start tablet:gap-5 gap-4 w-full pc:w-[1200px]">
+      <div className="flex flex-col justify-center items-start w-full pc:w-[1200px]">
         <div className="tablet:text-headline-2 text-title-2 font-semibold text-black-100">
           추운 겨울에 어울리는 향수 TOP 5
         </div>
--- src/components/domains/main/mainBanner/MainBannerPerfumeList.tsx
+++ src/components/domains/main/mainBanner/MainBannerPerfumeList.tsx
@@ -26,17 +26,17 @@ export const MainBannerPerfumeList = ({
           animation: left-right 1s linear infinite alternate;
         }
       `}</style> */}
-      <div className="flex gap-5 justify-between w-full px-5">
+      <div className="flex gap-5 justify-between w-full px-5 tablet:py-5 py-4">
         {data.map((item) => (
           <Link key={item.id} href={`/perfumes/${item.id}`}>
             <PerfumeCard
-              className="mobile:block hidden tablet:w-[180px] w-auto"
+              className="tablet:block hidden tablet:w-[180px] w-auto"
               perfumeImage={item.imageUrl || null}
               brandName={item.brand.nameKo || item.brand.nameEn || null}
               perfumeName={item.nameKo || item.nameEn || null}
             />
             <PerfumeCard
-              className="mobile:hidden block"
+              className="tablet:hidden block w-[80px] mobile:w-auto"
               cardType="smallSize"
               perfumeImage={item.imageUrl || null}
               brandName={item.brand.nameKo || item.brand.nameEn || null}
