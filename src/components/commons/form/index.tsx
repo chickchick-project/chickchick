5a65c70d47523dd3edb50ce885df4a2f7ea2ad32
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-07-02 16:22:24 +0900

    Feat: Form 컴포넌트 추가

---
+++ src/components/commons/form/index.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { ZodTypeAny } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DefaultValues,
+  FieldValues,
+  FormProvider,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+
+interface IForm<T extends FieldValues> {
+  children: React.ReactNode;
+  schema: ZodTypeAny;
+  defaultValues?: DefaultValues<T>;
+  useInitialize: (methods: UseFormReturn<T>) => {
+    onSubmit: (data: T) => Promise<void>;
+    isLoaded?: boolean;
+  };
+}
+
+export default function Form<T extends FieldValues>({
+  children,
+  schema,
+  defaultValues,
+  useInitialize,
+}: IForm<T>) {
+  const method = useForm<T>({
+    resolver: zodResolver(schema),
+    mode: "all",
+    defaultValues,
+  });
+
+  const { onSubmit, isLoaded = true } = useInitialize(method);
+
+  return (
+    <FormProvider {...method}>
+      <form
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onSubmit={method.handleSubmit(onSubmit)}
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {isLoaded ? children : <></>}
+      </form>
+    </FormProvider>
+  );
+}
