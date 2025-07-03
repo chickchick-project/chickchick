854f43838398482e21aeef16e98bfb25f9b54359
Joohyun Kim <trust987@naver.com>
2025-07-03 19:55:40 +0900

    Feat: 게시물 상세페이지 Service 구현

--- src/lib/queries/community/postQueries.ts
+++ src/lib/queries/community/postQueries.ts
@@ -1,4 +1,4 @@
-import { Post, PostCategory } from "@prisma/client";
+import { Post, PostCategory, Prisma } from "@prisma/client";
 import { validateUserSession } from "../userQueries";
 import { prisma } from "@/lib/prisma";
 
@@ -15,6 +15,20 @@ export type TPostFormData = Pick<
   "content" | "title" | "category" | "thumbnailUrl"
 >;
 
+export type TPostDetail = Omit<Post, "bookmarks" | "userId"> & {
+  author: {
+    id: string;
+    nickname: string;
+    imageUrl: string | null;
+  };
+  isAuthor: boolean;
+  isLiked: boolean;
+  bookmarkInfo: {
+    isBookmarked: boolean;
+    id: string | null;
+  };
+};
+
 export async function createCommunityPost(
   userId: string,
   postData: TPostFormData
@@ -26,11 +40,72 @@ export async function createCommunityPost(
       data: { ...postData, userId },
     });
     return { post, message: "커뮤니티 글이 작성되었습니다.", success: true };
-  } catch (error: any) {
+  } catch (error) {
     console.error(error);
     return {
-      message: "커뮤니티 글 작성에 실패했습니다." + error.message,
+      message: "커뮤니티 글 작성에 실패했습니다.",
       success: false,
     };
+  }
+}
+
+export async function getPostDetailByIdService(
+  userId: string | null,
+  postId: string
+): Promise<TPostDetail | null> {
+  try {
+    const result = await prisma.$transaction(async (tx) => {
+      const post = await tx.post.update({
+        where: { id: postId },
+        data: { viewCount: { increment: 1 } },
+        include: {
+          author: { select: { id: true, nickname: true, imageUrl: true } },
+          bookmarks: {
+            where: { userId: userId ?? undefined },
+          },
+        },
+      });
+
+      const likeActivity = userId
+        ? await tx.userActivity.findFirst({
+            where: {
+              userId: userId,
+              targetId: postId,
+              targetType: "POST",
+              action: "LIKED",
+            },
+          })
+        : null;
+
+      return { post, likeActivity };
+    });
+    if (!result || !result.post) {
+      return null;
+    }
+    const { post, likeActivity } = result;
+    const { bookmarks, userId: postUserId, ...postData } = post;
+    const isBookmarked = bookmarks.length > 0;
+    const isAuthor = userId === post.author.id;
+
+    return {
+      ...postData,
+      isAuthor,
+      isLiked: !!likeActivity,
+      bookmarkInfo: {
+        isBookmarked,
+        id: isBookmarked ? bookmarks[0].id : null,
+      },
+    };
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      console.error(`게시글(id: ${postId})을 찾을 수 없습니다.`);
+      return null;
+    } else {
+      console.error("에러:", error);
+      throw new Error("게시글을 가져오는데 실패했습니다.");
+    }
   }
 }
