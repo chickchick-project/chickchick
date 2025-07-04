6f519ce3ebc6b8d7dc8a4c9cc68d394a588e85e8
Yoon-kyungLee <dldbsrud225@gmail.com>
2025-07-04 18:04:34 +0900

    Feat: 리뷰 생성 및 조회 API 구현(hono 적용), 스키마 정의 추가

---
+++ src/lib/routes/v1/reviews.handler.ts
@@ -0,0 +1,33 @@
+import { createReviewRoute } from "@/lib/api-spec/v1/review.routes";
+import { createReviewService } from "@/lib/services/review.service";
+import { OpenAPIHono } from "@hono/zod-openapi";
+
+const reviewsApi = new OpenAPIHono();
+
+reviewsApi.openapi(
+  createReviewRoute,
+  async (c) => {
+    const newReview = c.req.valid("json");
+    const createdReview = await createReviewService(newReview);
+    return c.json(
+      {
+        success: true,
+        message: "Review created successfully",
+        data: createdReview,
+      },
+      201
+    );
+  },
+  (result, c) => {
+    if (!result.success) {
+      return c.json(
+        {
+          success: false,
+          message: "Review creation failed",
+          error: result.error,
+        },
+        400
+      );
+    }
+  }
+);
---
+++ src/lib/services/review.service.ts
@@ -0,0 +1,25 @@
+import { CreateReview, Review } from "../schemas/review.schema";
+
+const fakeDb: Review[] = [];
+
+export async function createReviewService(
+  newReview: CreateReview
+): Promise<Review> {
+  const createdReview: Review = {
+    ...newReview,
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+  fakeDb.push(createdReview);
+
+  console.log(fakeDb);
+  return createdReview;
+}
+
+export async function getReviewService(
+  perfumeId: string
+): Promise<Review | null> {
+  const review = fakeDb.find((u) => u.id === perfumeId);
+  return review || null;
+}
