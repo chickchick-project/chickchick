1a290d57687a3511e19db570e75f1a9653d18ea9
Joohyun Kim <trust987@naver.com>
2025-07-10 16:26:41 +0900

    Refactor: 드롭다운 관련 컴포넌트 리팩토링

--- src/components/commons/dropdown/SortDropdown.tsx
+++ src/components/commons/dropdown/SortDropdown.tsx
@@ -1,21 +1,26 @@
 "use client";
-import { DEFAULT_SORT_BY, Option, SORT_BY_OPTIONS, TSortBy } from "@/lib/constants/options";
-import { useState } from "react";
+import {
+  DEFAULT_SORT_BY,
+  Option,
+  SORT_BY_OPTIONS,
+  TSortBy,
+} from "@/lib/constants/options";
 import Dropdown from "./DropdownBase";
 
 export interface ISortDropdownProps {
-  currentOption?: TSortBy;
+  currentOption?: TSortBy | null;
   type: "perfume" | "community";
+  onSortChange: (option: Option) => void;
 }
 
-export default function SortDropdown({ currentOption, type }: ISortDropdownProps) {
-  const [selectedOption, setSelectedOption] = useState<Option>(
-    () => SORT_BY_OPTIONS[type].find((option) => option.value === currentOption) || DEFAULT_SORT_BY[type]
-  );
-
-  const handleSortChange = (option: Option) => {
-    setSelectedOption(option);
-  };
+export default function SortDropdown({
+  currentOption,
+  type,
+  onSortChange,
+}: ISortDropdownProps) {
+  const selectedOption =
+    SORT_BY_OPTIONS[type].find((option) => option.value === currentOption) ||
+    DEFAULT_SORT_BY[type];
 
   return (
     <>
@@ -23,8 +28,8 @@ export default function SortDropdown({ currentOption, type }: ISortDropdownProps
         selectedOption={selectedOption}
         currentOption={currentOption}
         options={SORT_BY_OPTIONS[type]}
-        handleChangeOption={handleSortChange}
-        id={""}
+        handleChangeOption={onSortChange}
+        id="sort-dropdown"
       />
     </>
   );
