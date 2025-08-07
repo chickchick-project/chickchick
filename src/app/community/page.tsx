a19867fc6bebe911e811276a97dfbb6bfd20c46e
kuan <guen9310@gmail.com~>
2025-08-07 11:14:22 +0900

    Fix: Build 시, community 페이지 에러 수정

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,9 +1,11 @@
 import PageClient from "@/components/domains/community/PageClient";
+import { Suspense } from "react";
 
 export default function Page() {
+  const ClientFallback = () => <div>로딩 중...</div>;
   return (
-    <>
+    <Suspense fallback={<ClientFallback />}>
       <PageClient />
-    </>
+    </Suspense>
   );
 }
