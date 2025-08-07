128d4d2b8ce8cdc883c36a5cbff630895a795e72
Joohyun Kim <trust987@naver.com>
2025-08-07 19:05:28 +0900

    Feat: 선택적 인증을 위한 미들웨어 추가

--- src/lib/hono/middleware/auth.middleware.ts
+++ src/lib/hono/middleware/auth.middleware.ts
@@ -13,3 +13,16 @@ export const authMiddleware = createMiddleware<AppContext>(async (c, next) => {
 
   await next();
 });
+
+export const optionalAuthMiddleware = createMiddleware<AppContext>(
+  async (c, next) => {
+    const session = await getSession();
+
+    if (session && session.user) {
+      c.set("user", session.user);
+      c.set("session", session);
+    }
+
+    await next();
+  }
+);
