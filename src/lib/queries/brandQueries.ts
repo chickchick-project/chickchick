fcf9319f40210e179f01789db0cc8c7a50b777ea
kuan <guen9310@gmail.com~>
2025-08-28 19:27:10 +0900

    Chore: 더 이상 사용하지 않는 api 제거

--- src/lib/queries/brandQueries.ts
+++ src/lib/queries/brandQueries.ts
@@ -1,30 +1,30 @@
-import { supabase } from "@/lib/supabase/init";
-import { prisma } from "@/lib/prisma";
+// import { supabase } from "@/lib/supabase/init";
+// import { prisma } from "@/lib/prisma";
 
-/**
- * 모든 브랜드 목록 조회
- */
-export async function fetchAllBrands() {
-  const data = await prisma.brand.findMany();
+// /**
+//  * 모든 브랜드 목록 조회
+//  */
+// export async function fetchAllBrands() {
+//   const data = await prisma.brand.findMany();
 
-  if (!data) {
-    throw new Error("브랜드를 찾을 수 없습니다.");
-  }
+//   if (!data) {
+//     throw new Error("브랜드를 찾을 수 없습니다.");
+//   }
 
-  return data;
-}
+//   return data;
+// }
 
-/**
- * 특정 브랜드 ID로 브랜드 상세 조회
- */
-export async function fetchBrandWithPerfumes(brandName: string) {
-  const { data, error } = await supabase.rpc("get_brand_with_perfumes", {
-    brand_name: brandName,
-  });
+// /**
+//  * 특정 브랜드 ID로 브랜드 상세 조회
+//  */
+// export async function fetchBrandWithPerfumes(brandName: string) {
+//   const { data, error } = await supabase.rpc("get_brand_with_perfumes", {
+//     brand_name: brandName,
+//   });
 
-  if (error) {
-    throw new Error(error.message);
-  }
+//   if (error) {
+//     throw new Error(error.message);
+//   }
 
-  return data;
-}
+//   return data;
+// }
