ae75c9c037ed530656533e9aed89da809bc485f4
Joohyun Kim <trust987@naver.com>
2025-08-28 16:49:28 +0900

    Feat: 게시글 작성을 위한 향수 검색 로직 구현

---
+++ src/components/domains/post/form/postRelatedPerfume/usePerfumeSearch.ts
@@ -0,0 +1,60 @@
+import { PerfumeBaseResponse } from "@/lib/hono/schemas/perfume.schema";
+import { useState } from "react";
+
+export default function usePerfumeSearch() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<PerfumeBaseResponse[]>([]);
+
+  const [tempSelectedPerfumeIds, setTempSelectedPerfumesIds] = useState<
+    string[]
+  >([]);
+  const [selectedPerfumes, setSelectedPerfumes] = useState<
+    PerfumeBaseResponse[]
+  >([]);
+
+  const handleToggleTempSelect = (perfume: PerfumeBaseResponse) => {
+    setTempSelectedPerfumesIds((prev) => {
+      if (prev.includes(perfume.id)) {
+        return prev.filter((id) => id !== perfume.id);
+      } else {
+        return [...prev, perfume.id];
+      }
+    });
+  };
+
+  const handleAddSelectedPerfumes = () => {
+    const newPerfumesToAdd = searchResults.filter((perfume) =>
+      tempSelectedPerfumeIds.includes(perfume.id)
+    );
+    setSelectedPerfumes((prev) => {
+      const existingIds = prev.map((p) => p.id);
+      const filteredNewPerfumes = newPerfumesToAdd.filter(
+        (p) => !existingIds.includes(p.id)
+      );
+
+      return [...prev, ...filteredNewPerfumes];
+    });
+
+    setSearchQuery("");
+    setSearchResults([]);
+    setTempSelectedPerfumesIds([]);
+  };
+
+  const handleRemoveSelectedPerfume = (perfumeId: string) => {
+    setSelectedPerfumes((prev) =>
+      prev.filter((perfume) => perfume.id !== perfumeId)
+    );
+  };
+
+  return {
+    searchQuery,
+    setSearchQuery,
+    searchResults,
+    setSearchResults,
+    selectedPerfumes,
+    tempSelectedPerfumeIds,
+    handleToggleTempSelect,
+    handleAddSelectedPerfumes,
+    handleRemoveSelectedPerfume,
+  };
+}
