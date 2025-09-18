a7080176dc48fa7fee778feeca77fa2df707acc8
Joohyun Kim <trust987@naver.com>
2025-09-18 19:58:31 +0900

    Chore: 상세페이지 세부 스타일 수정

--- src/components/commons/ckeditor5/EditorLoading.tsx
+++ src/components/commons/ckeditor5/EditorLoading.tsx
@@ -1,5 +1,5 @@
 export default function EditorPlaceholder() {
   return (
-    <div className="w-full min-h-[640px] bg-gray-300 animate-pulse rounded-[0.5rem]" />
+    <div className="w-full min-h-[440px] tablet:min-h-[640px] bg-gray-300 animate-pulse rounded-[0.5rem]" />
   );
 }
--- src/components/domains/post/header/index.tsx
+++ src/components/domains/post/header/index.tsx
@@ -2,7 +2,7 @@ import CautionCard from "./CautionCard";
 
 export default function Header() {
   return (
-    <header className="w-full my-10">
+    <header className="w-full mt-6  tablet:my-10">
       <h1 className="mb-10 text-black-100 font-bold text-headline-2 tablet:text-headline-1 text-center">
         글 작성
       </h1>
