4b32138bb6f1ef9c7046c4ffed2f44ea8ecfd027
Joohyun Kim <trust987@naver.com>
2025-09-25 20:26:47 +0900

    Chore: 빌드 오류 수정

--- src/components/domains/postDetail/commentSection/comment/CommentAuthInfo.tsx
+++ src/components/domains/postDetail/commentSection/comment/CommentAuthInfo.tsx
@@ -1,38 +1,20 @@
-// import { ICommentAuthInfoProps } from "./postComment.types";
-// import AuthorProfile from "@/components/commons/author/AuthorProfile";
+import { ICommentAuthInfoProps } from "./postComment.types";
+import AuthorProfile from "@/components/commons/author/AuthorProfile";
 
-// export function CommentAuthInfo({
-//   author,
-//   profileImage,
-//   size,
-//   isPostAuthor,
-// }: ICommentAuthInfoProps) {
-//   return (
-// <<<<<<< feature/me-collection
-//     <>
-//       <AuthorInfo
-//         info={{
-//           type: "comment",
-//         }}
-//         author={{
-//           id: "unknown",
-//           nickname: author,
-//           imageUrl: profileImage || "",
-//         }}
-//         createdAt={new Date(createdAt)}
-//         isAuthor={false}
-//         // size="large"
-//       />
-//     </>
-// =======
-//     <div className="text-black-200 flex items-center justify-center gap-2">
-//       <AuthorProfile name={author} profileImage={profileImage} size={size} />
-//       {isPostAuthor && (
-//         <div className="text-label-5 text-primary-100 border rounded-full px-1 border-primary-100 ">
-//           작성자
-//         </div>
-//       )}
-//     </div>
-// >>>>>>> dev
-//   );
-// }
+export function CommentAuthInfo({
+  author,
+  profileImage,
+  size,
+  isPostAuthor,
+}: ICommentAuthInfoProps) {
+  return (
+    <div className="text-black-200 flex items-center justify-center gap-2">
+      <AuthorProfile name={author} profileImage={profileImage} size={size} />
+      {isPostAuthor && (
+        <div className="text-label-5 text-primary-100 border rounded-full px-1 border-primary-100 ">
+          작성자
+        </div>
+      )}
+    </div>
+  );
+}
