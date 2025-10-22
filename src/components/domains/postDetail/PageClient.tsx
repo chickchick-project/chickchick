e37c392ed2ed88cd775d170e4c68173e666ba0e7
Joohyun Kim <trust987@naver.com>
2025-10-23 00:22:16 +0900

    Refactor: 게시글 상세페이지 로딩 및 에러 상태 처리

--- src/components/domains/postDetail/PageClient.tsx
+++ src/components/domains/postDetail/PageClient.tsx
@@ -10,23 +10,37 @@ import {
 } from "./postDetail.helpers";
 import { useLogRecentItem } from "@/lib/stores/useLogRecentItem";
 import { useRecentPostsStore } from "@/lib/stores/useRecentPostsStore";
+import { Spinner } from "@/components/commons/loading/Spinner";
 
 export default function PageClient({ postId }: { postId: string }) {
-  const { data: postDetail } = useQuery({
+  const {
+    data: postDetail,
+    isPending: isPostDetailPending,
+    isError: isPostDetailError,
+    error: postDetailError,
+  } = useQuery({
     queryKey: ["post", postId],
     queryFn: () => getPostDetailById(postId),
     staleTime: 1000 * 60 * 5,
   });
 
-  const { data: postStatus } = useQuery({
+  const {
+    data: postStatus,
+    isPending: isPostStatusPending,
+    isError: isPostStatusError,
+    error: postStatusError,
+  } = useQuery({
     queryKey: ["post", postId, "status"],
     queryFn: () => getPostDetailStatusById(postId),
   });
 
   useLogRecentItem(postDetail, useRecentPostsStore);
-
-  if (!postDetail || !postStatus) {
-    return <div>Loading...</div>;
+  const error = postDetailError || postStatusError;
+  if (isPostDetailPending || isPostStatusPending) {
+    return <Spinner />;
+  }
+  if (isPostDetailError || isPostStatusError) {
+    return <div>{error?.message}</div>;
   }
   const { content, ...postDetailHeader } = postDetail;
 
