ea6ad421810a634f0b352dbe1a534ba4378eaf40
Joohyun Kim <148737398+kimbobby@users.noreply.github.com>
2025-10-30 19:06:27 +0900

    Refactor: 게시글 상세 페이지 관련 로직 개선(북마크, 좋아요, 게시글 삭제) (#108)
    * Refactor: 커뮤니티 게시글 북마크, 좋아요 TanStack Query로 수정
    
    * Chore: 사용하지 않는 파일 삭제
    
    * Refactor: postDetail 헬퍼함수 apiClient로 리팩토링
    
    * Refactor: 댓글 및 커뮤니티 서비스 안정성 강화 (게시글 published 확인 및 에러 처리 로직 개선)
    
    * Refactor: 게시글 상세페이지 로딩 및 에러 상태 처리
    
    * Feat: 상세페이지 게시판 칩 클릭 시 해당 게시판 목록 페이지 이동
    
    * Refactor: 커뮤니티 게시글 삭제 헬퍼 함수 apiClient로 수정
    
    * Feat: usePostMutation 훅에 게시글 삭제 기능 및 캐시 관리 추가
    
    * Refactor:  usePostMutation 훅을 사용하여 게시글 삭제 로직 리팩토링
    
    * Refactor: 게시글 작성 헬퍼 함수 apiClient로 수정

--- src/components/domains/postDetail/header/PostInteractions.tsx
+++ src/components/domains/postDetail/header/PostInteractions.tsx
@@ -1,77 +1,41 @@
 "use client";
-import { useEffect, useState } from "react";
+
 import { BookmarkIcon } from "@/components/commons/interactions/icons/BookmarkIcon";
 import { LikeIcon } from "@/components/commons/interactions/icons/LikeIcon";
 import { ShareIcon } from "@/components/commons/interactions/icons/ShareIcon";
 import { Interactions } from "@/components/commons/interactions";
-import { useRouter } from "next/navigation";
-import {
-  toggleLikedPostById,
-  toggleBookmarkedPostById,
-} from "../postDetail.helpers";
-import { useUserStore } from "@/lib/stores/useUserStore";
+import usePostInteractionMutation from "./usePostInteractionMutation";
 
 interface IPostInteractionsProps {
-  initialIsLiked: boolean;
-  initialIsBookmarked: boolean;
+  isLiked: boolean;
+  isBookmarked: boolean;
   postId: string;
 }
 
 export default function PostInteractions({
-  initialIsLiked,
-  initialIsBookmarked,
+  isLiked,
+  isBookmarked,
   postId,
 }: IPostInteractionsProps) {
-  const router = useRouter();
-  const { user, isLoading } = useUserStore();
-
-  const [isLiked, setIsLiked] = useState(initialIsLiked);
-  const [isBookmarked, setIsBookmarked] = useState(initialIsBookmarked);
-
-  const handleLikeToggle = async () => {
-    if (!user) {
-      alert("로그인이 필요한 기능입니다.");
-      return;
-    }
-    setIsLiked((prev) => !prev);
-    try {
-      await toggleLikedPostById(postId);
-      router.refresh();
-    } catch (error) {
-      setIsLiked((prev) => !prev);
-      alert("좋아요 처리실패.");
-      console.error("좋아요 처리 실패:", error);
-    }
-  };
-
-  const handleBookmarkToggle = async () => {
-    if (!user) {
-      alert("로그인이 필요한 기능입니다.");
-      return;
-    }
-    setIsBookmarked((prev) => !prev);
-    try {
-      await toggleBookmarkedPostById(postId);
-      router.refresh();
-    } catch (error) {
-      setIsBookmarked((prev) => !prev);
-      alert("북마크 처리 실패.");
-      console.error("북마크 처리 실패:", error);
-    }
-  };
+  const { toggleBookmarkMutation, toggleLikeMutation } =
+    usePostInteractionMutation(postId);
+  const { isPending: isBookmarkPending } = toggleBookmarkMutation;
+  const { isPending: isLikePending } = toggleLikeMutation;
 
   const interactions = [
     {
       type: "like",
       isActive: isLiked,
-      onClick: handleLikeToggle,
+      onClick: () => toggleLikeMutation.mutate(),
+      disabled: isLikePending,
       label: "좋아요",
       icon: <LikeIcon isActive={isLiked} />,
     },
     {
       type: "bookmark",
       isActive: isBookmarked,
-      onClick: handleBookmarkToggle,
+      onClick: () => toggleBookmarkMutation.mutate(),
+      disabled: isBookmarkPending,
       label: "북마크",
       icon: <BookmarkIcon isActive={isBookmarked} />,
     },
@@ -82,13 +46,6 @@ export default function PostInteractions({
       icon: <ShareIcon />,
     },
   ];
-
-  useEffect(() => {
-    if (!isLoading && !user) {
-      setIsLiked(false);
-      setIsBookmarked(false);
-    }
-  }, [user, isLoading]);
 
   return (
     <>
--- src/components/domains/postDetail/header/index.tsx
+++ src/components/domains/postDetail/header/index.tsx
@@ -7,6 +7,7 @@ import {
   ApiPostDetailResponse,
   ApiPostStatusResponse,
 } from "@/lib/hono/schemas/community.schema";
+import Link from "next/link";
 
 type PostDetailHeaderProps = Omit<ApiPostDetailResponse, "content"> & {
   postStatus: ApiPostStatusResponse;
@@ -34,10 +35,12 @@ export default function PostDetailHeader(props: PostDetailHeaderProps) {
   return (
     <header className="mobile:mt-10 pc:mt-[60px] px-4">
       <div className="flex item-center justify-between">
-        <BoardChip type={category} />
+        <Link href={`/community?tab=${category}`}>
+          <BoardChip type={category} />
+        </Link>
         <PostInteractions
-          initialIsLiked={isLiked}
-          initialIsBookmarked={isBookmarked}
+          isLiked={isLiked}
+          isBookmarked={isBookmarked}
           postId={postId}
         />
       </div>
